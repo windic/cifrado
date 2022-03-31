510e232f1d9b2f33925cb304231221ba081e1c94
Sanchez Catalan <esanchezcatalan@deloitte.es>
2022-03-31 16:40:17 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def ejercicio1():
+    m = ""
+    mater_key = ""
+
+    pass
+
+def ejercicio2():
+    pass
+
+if __name__ == "__main__":
+    print("------------------------Ejercicio 1---------------------\n")
+    ejercicio1()
+    print("\n------------------------Ejercicio 2---------------------\n")
+
+
+    pass
